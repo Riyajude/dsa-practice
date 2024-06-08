ecf40a32ffae7ba1a6e43c945f8749532ff9c132
Riya Mary Jude <97887337+Riyajude@users.noreply.github.com>
2024-06-09 00:36:07 +0530

    Create clockwiserotarray.py
    clockwise rotation by 1 position

---
+++ clockwiserotarray.py
@@ -0,0 +1,20 @@
+def rotate( arr, n):
+    l=arr.pop()
+    arr.insert(0,l)
+    
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
