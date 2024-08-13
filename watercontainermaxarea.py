55f5609dd9ff362f3ff465074d0032e4d208f170
Riya Mary Jude <97887337+Riyajude@users.noreply.github.com>
2024-08-13 21:16:18 +0530

    Update watercontainermaxarea.py

--- watercontainermaxarea.py
+++ watercontainermaxarea.py
@@ -1,4 +1,8 @@
-
+#initialise 2 pointers left at 0 and right at lenght-1
+#iterate as long as left<right
+#calculate current area as min value of element at left and right multiplied with the width=right-left
+#calculate actual area as max value of area and current area
+#if element at left<right then increment left, else decrement right, Finally return area
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
