5da1c91808ecfa2106e5de95498ef53fd00bd657
Riya Mary Jude <97887337+Riyajude@users.noreply.github.com>
2024-09-20 09:33:04 +0530

    Create remove*fromstring.cpp

---
+++ remove*fromstring.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string removeStars(string s) {
+        string result;
+        for (char ch : s) {
+            if (ch == '*') {
+                if (!result.empty()) {
+                    result.pop_back();  
+                }
+            } else {
+                result.push_back(ch);  
+            }
+        }
+        return result;
+    }
+};
